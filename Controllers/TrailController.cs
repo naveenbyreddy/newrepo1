37ac10bfafeb76c6073b6a06c6ba5307e823a354
Naveen kumar reddy byreddy <byreddy.naveen081@gmail.com>
2021-06-03 15:41:44 +0530

    pushup

---
+++ Controllers/TrailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace studentdetails.Controllers
+{
+    public class TrailController : Controller
+    {
+        // GET: Trail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
